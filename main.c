f5b32f804fe404d0dda1808d85682bab541bd498
Daniel Umpierrez <10012416+havocesp@users.noreply.github.com>
2025-01-07 09:46:32 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xutil.h>
+#include "util.h"
+#include "selection.h"
+#include "xwindow.h"
+
+int main(int argc, char *argv[]) {
+    // (Move main function and argument parsing here)
+}
